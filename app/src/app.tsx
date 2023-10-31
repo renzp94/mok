9cfb50634e9f7ccf42125ad19045efae08fa863f
renzp94 <1074720760@qq.com>
2023-10-31 15:51:48 +0800

    feat: 添加版本更新检测

--- app/src/app.tsx
+++ app/src/app.tsx
@@ -4,13 +4,50 @@ import Taro, { useLaunch } from '@tarojs/taro'
 import './app.less'
 
 function App(props) {
-  useLaunch(() => {
-    const token = storage.get(TOKEN)
+  useLaunch(async () => {
+    const updateManager = Taro.getUpdateManager()
+    const { hasUpdate } = await new Promise<{
+      hasUpdate: boolean
+    }>((resolve) => updateManager.onCheckForUpdate(resolve))
 
-    if (token) {
-      Taro.reLaunch({
-        url: '/pages/home/index',
+    if (hasUpdate) {
+      Taro.showModal({
+        title: '更新提示',
+        content: '检测到新版本，是否更新?',
+        success: async ({ confirm }) => {
+          if (confirm) {
+            try {
+              Taro.showLoading({ title: '下载中...' })
+              await new Promise((resolve, reject) => {
+                updateManager.onUpdateReady(resolve)
+                updateManager.onUpdateFailed(reject)
+              })
+
+              Taro.showModal({
+                title: '更新提示',
+                content: '新版本已经准备好，是否重启应用？',
+                success: function (res) {
+                  if (res.confirm) {
+                    updateManager.applyUpdate()
+                  }
+                },
+              })
+            } catch {
+              Taro.showToast({ title: '下载失败，请稍后重试', icon: 'none' })
+            } finally {
+              Taro.hideLoading()
+            }
+          }
+        },
       })
+    } else {
+      const token = storage.get(TOKEN)
+
+      if (token) {
+        Taro.reLaunch({
+          url: '/pages/home/index',
+        })
+      }
     }
   })
 
